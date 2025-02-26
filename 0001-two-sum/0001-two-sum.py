d1debbb9ee57ba42176340d875253afbf54a28b1
ge-ogre <georges.elizee@gmail.com>
2025-02-26 17:18:44 -0500

    Time: 4 ms (42.12%), Space: 18.9 MB (50.99%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        left, right = 0, 1
-        while left < right:
-            if nums[left] + nums[right] == target:
-                return [left, right]
-            else:
-                if right >= len(nums) - 1:
-                    left += 1
-                    right = left + 1
-                else:
-                    right += 1
+        store = {}
+        for i, n in enumerate(nums):
+            store[n] = i
+        
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in store and store[diff] != i:
+                return [i, store[diff]]
